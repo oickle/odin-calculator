d49ae1f149291d40f4607d21c92a42d7af284636
oickle <oycell@aol.com>
2023-08-08 19:31:26 -0400

    Create empty onclick functions

---
+++ main.js
@@ -0,0 +1,32 @@
+function setSign() {
+
+}
+
+function setPercent() {
+    
+}
+
+function setDecimal() {
+
+}
+
+function divide() {
+
+}
+
+function multiply() {
+
+}
+
+function subtract() {
+
+}
+
+function add() {
+
+}
+
+
+function getResult() {
+
+}
