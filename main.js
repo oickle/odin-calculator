045175c3c8950032fd06f3e04c37282502ff3f9f
oickle <oycell@aol.com>
2023-08-09 18:33:35 -0400

    Add number typing functionality

--- main.js
+++ main.js
@@ -1,5 +1,33 @@
-function setSign() {
+let previous = null;
+let current = ""
+let result = 0;
 
+let historyArray = [];
+
+let operation = 'none';
+
+let numberButtons = document.querySelectorAll('.number');
+
+let results = document.querySelector('.results')
+let history = document.querySelector('.history');
+
+function addListeners() {
+    numberButtons.forEach(button => {
+        button.addEventListener('click', addDigit);
+    });
+}
+
+function addDigit(e) {
+    const buttonLabel = e.target.textContent;
+    current = current.concat(buttonLabel);
+    results.textContent = current;
+}
+
+function setSign() {
+    current *= -1;
+    current = current.toString(); // has to be string for non-math sections
+
+    results.textContent = current;
 }
 
 function setPercent() {
@@ -7,7 +35,11 @@ function setPercent() {
 }
 
 function setDecimal() {
+    // if there is already a decimal, remove it.
+    if (current.includes('.')) current = current.replace('.', '');
+    else current = current.concat('.'); // otherwise, add one
 
+    results.textContent = current;
 }
 
 function divide() {
@@ -26,10 +58,30 @@ function add() {
 
 }
 
-function clear() {
-    
+function erase() {
+    if (current != "") { // if there is a current number, empty it
+        current = "";
+    }
+    else if (current === "" && previous !== null) {
+        previous = null;
+    }
+
+    results.textContent = "0";
+}
+
+function updateHistory() {
+    history.textContent = historyArray.join().replace(',', ' ');
 }
 
 function getResult() {
-
+    switch (operation) {
+        case 'add':
+            
+            break;
+        default:
+            
+            break;
+    }
 }
+
+addListeners();
