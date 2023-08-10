6f9c21b24bc722392964798ea51180dc43cbea49
oickle <oycell@aol.com>
2023-08-09 20:32:44 -0400

    Add negative, decimal, and clear functionality

--- main.js
+++ main.js
@@ -1,33 +1,41 @@
-let previous = null;
-let current = ""
+let previous = 0;
+let current = 0;
 let result = 0;
+let operator = '';
 
 let historyArray = [];
 
-let operation = 'none';
 
 let numberButtons = document.querySelectorAll('.number');
 
 let results = document.querySelector('.results')
 let history = document.querySelector('.history');
 
+let eraseButton = document.querySelector('.erase');
+
 function addListeners() {
     numberButtons.forEach(button => {
         button.addEventListener('click', addDigit);
     });
+
+    eraseButton.addEventListener('click', erase);
 }
 
 function addDigit(e) {
+    eraseButton.textContent = 'C';
+
+    if (current == 0) current = ''; // set to empty string temporarily to concat
+
     const buttonLabel = e.target.textContent;
-    current = current.concat(buttonLabel);
-    results.textContent = current;
+    current = +(`${current}${buttonLabel}`); // concats number on clicked button to previous number
+
+    updateDisplay(current);
 }
 
 function setSign() {
     current *= -1;
-    current = current.toString(); // has to be string for non-math sections
 
-    results.textContent = current;
+    updateDisplay(current);
 }
 
 function setPercent() {
@@ -36,52 +44,56 @@ function setPercent() {
 
 function setDecimal() {
     // if there is already a decimal, remove it.
-    if (current.includes('.')) current = current.replace('.', '');
-    else current = current.concat('.'); // otherwise, add one
+    if (current.toString().includes('.')) current = +current.toString().replace('.', '');
+    else current = current.toString().concat('.'); // otherwise, add one
 
-    results.textContent = current;
-}
-
-function divide() {
-
-}
-
-function multiply() {
-
-}
-
-function subtract() {
-
+    updateDisplay(current);
 }
 
 function add() {
 
 }
 
+function subtract(a, b) {
+
+}
+
+function multiply(a, b) {
+
+}
+
+function divide() {
+
+}
+
 function erase() {
-    if (current != "") { // if there is a current number, empty it
-        current = "";
+    if (current != 0) { // if there is a current number, empty it
+        current = 0;
     }
-    else if (current === "" && previous !== null) {
-        previous = null;
+    else if ((previous != 0)) { // wipe everything else that is left
+        previous = 0;
+        historyArray = []
     }
 
-    results.textContent = "0";
+    eraseButton.textContent = (current == 0 && previous == 0) ? 'AC' : 'C';
+
+    updateDisplay(current);
 }
 
-function updateHistory() {
-    history.textContent = historyArray.join().replace(',', ' ');
+function updateDisplay(display) {
+    results.textContent = display;
+
+    let newHistory = '';
+    for (x in historyArray) {
+        newHistory = newHistory.concat(historyArray[x].toString(), ' ');
+    }
+    history.textContent = newHistory;
 }
 
-function getResult() {
-    switch (operation) {
-        case 'add':
-            
-            break;
-        default:
-            
-            break;
-    }
+// takes an operator and two numbers
+function operate(a, b) {
+         
+    updateDisplay();
 }
 
 addListeners();
